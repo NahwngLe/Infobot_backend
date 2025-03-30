fd845df3b4928f0d7530c8f21e7c339a1f3f7665
unknown <lequynhan1212004@gmail.com>
2025-03-30 12:46:46 +0700

    Change api /upload

--- app/routers/pdf.py
+++ app/routers/pdf.py
@@ -5,15 +5,16 @@ from pdf2image import convert_from_bytes
 import io
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+import shutil
 import gridfs
 from bson import ObjectId
 import hashlib
 from app.utils.handlePdf import pdfBytesToText
+from app.utils.flow_from_pdf_to_quiz import *
 
+#MongoDb
 uri = "mongodb+srv://nhanlequy12:nhanhero09@nhancluster.rfxde.mongodb.net/your_database?retryWrites=true&w=majority&tlsAllowInvalidCertificates=true"
-# Create a new client and connect to the server
 client = MongoClient(uri)
-# , server_api=ServerApi('1')
 db = client["Infobot"]
 fs = gridfs.GridFS(db)
 
@@ -21,45 +22,17 @@ router = APIRouter(prefix="/pdf", tags=["PDF"])
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 @router.post("/upload")
-async def upload_pdf (file: UploadFile = File(...)):
-    if not file.filename.endswith(".pdf"):
+def upload_pdf_and_save(file: UploadFile = File(...)):
+    if file.filename.endswith(".pdf"):
         raise HTTPException(status_code=400, detail="File must be pdf")
-    
-    try:
-        pdf_bytes = await file.read()
-        file_hash = hashlib.md5(pdf_bytes).hexdigest()
-        
-        #Kiểm tra file có tồn tại chưa
-        existing_file = db.pdf_hash.find_one({"file_hash" : file_hash})
-        if existing_file:
-            return {
-                "message": "File already exists",
-                "filename": existing_file["filename"],
-                "pdf_id": str(existing_file["pdf_id"]),
-                "content": existing_file["content"],    
-            }
-            
-        pdf_id = fs.put(pdf_bytes, filename=file.filename)
-        
-        texts, lang = pdfBytesToText(pdf_bytes)
 
-        db.pdf_hash.insert_one({
-            "filename": file.filename,
-            "file_hash": file_hash,
-            "pdf_id" : pdf_id,
-            "content": texts,
-        })
-        
-        return {
-            "message": "Add file success",
-            "filename": file.filename,
-            "content": texts,
-            "language": lang,
-            "pdf_id": str(pdf_id)
-        }
-        
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+    temp_file_path = f"temp_{file.filename}"
+    with open(temp_file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    message = save_to_db(file)
+
+    return message
 
 @router.get("/get-pdf/{pdf_id}")
 async def get_pdf(pdf_id: str):
