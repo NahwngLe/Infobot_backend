b18a419edc5e993ad5714e0e79f99fbf7a4741aa
unknown <lequynhan1212004@gmail.com>
2025-03-30 14:24:05 +0700

    Update full api /pdf/upload

--- app/routers/pdf.py
+++ app/routers/pdf.py
@@ -6,7 +6,6 @@ import io
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import shutil
-import gridfs
 from bson import ObjectId
 import hashlib
 from app.utils.handlePdf import pdfBytesToText
@@ -16,28 +15,31 @@ from app.utils.flow_from_pdf_to_quiz import *
 uri = "mongodb+srv://nhanlequy12:nhanhero09@nhancluster.rfxde.mongodb.net/your_database?retryWrites=true&w=majority&tlsAllowInvalidCertificates=true"
 client = MongoClient(uri)
 db = client["Infobot"]
-fs = gridfs.GridFS(db)
 
 router = APIRouter(prefix="/pdf", tags=["PDF"])
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+# pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 @router.post("/upload")
-def upload_pdf_and_save(file: UploadFile = File(...)):
-    if file.filename.endswith(".pdf"):
+async def upload_pdf_and_save(file: UploadFile = File(...)) :
+    if not file.filename.endswith(".pdf"):
         raise HTTPException(status_code=400, detail="File must be pdf")
-
-    temp_file_path = f"temp_{file.filename}"
-    with open(temp_file_path, "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
-
-    message = save_to_db(file)
-
-    return message
-
-@router.get("/get-pdf/{pdf_id}")
-async def get_pdf(pdf_id: str):
     try:
-        file = fs.get(ObjectId(pdf_id))
-        return Response(content=file.read(), media_type="application/pdf")
+        temp_file_path = f"{file.filename}"
+        with open(temp_file_path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+
+        message = await save_to_db(temp_file_path, file)
+
+        return message
+
     except Exception as e:
-        raise HTTPException(status_code=404, detail="File not found")
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+# @router.get("/get-pdf/{pdf_id}")
+# async def get_pdf(pdf_id: str):
+#     try:
+#         file = fs.get(ObjectId(pdf_id))
+#         return Response(content=file.read(), media_type="application/pdf")
+#     except Exception as e:
+#         raise HTTPException(status_code=404, detail="File not found")
