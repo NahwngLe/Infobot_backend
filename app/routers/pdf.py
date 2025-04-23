698d92ed539b303b76969551c929edb52a24b614
unknown <lequynhan1212004@gmail.com>
2025-04-23 20:32:06 +0700

    Remove some lib

--- app/routers/pdf.py
+++ app/routers/pdf.py
@@ -1,16 +1,9 @@
 from fastapi import APIRouter, UploadFile, File, HTTPException, Response
-from PyPDF2 import PdfReader
-import pytesseract
-from pdf2image import convert_from_bytes
-import io
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
+
 from fastapi.responses import StreamingResponse
-import json
 import shutil
 from bson import ObjectId
-import hashlib
-from app.utils.handlePdf import pdfBytesToText
+
 from app.utils.flow_from_pdf_to_quiz import *
 
 #MongoDb
@@ -26,7 +19,7 @@ async def upload_pdf_and_save(file: UploadFile = File(...)) :
     if not file.filename.endswith(".pdf"):
         raise HTTPException(status_code=400, detail="File must be pdf")
     try:
-        temp_file_path = f"{file.filename}"
+        temp_file_path = f"app/assest/pdf/{file.filename}"
         with open(temp_file_path, "wb") as buffer:
             shutil.copyfileobj(file.file, buffer)
 
