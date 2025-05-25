68990fd8cf7fa2617da01645704293a3c8454c03
FPT <lequynhan1212004@gmail.com>
2025-05-25 22:53:48 +0700

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,6 +1,5 @@
 import gridfs
 from pinecone import Pinecone
-import google.generativeai as genai
 from pymongo.mongo_client import MongoClient
 from .config import *
 
@@ -12,7 +11,5 @@ client = MongoClient(uri)
 db = client["Infobot"]
 fs = gridfs.GridFS(db)
 
-# Generative AI
-genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel(model_name="gemini-2.0-flash")
+
 
