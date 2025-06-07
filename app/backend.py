5cab69c29d7a47f42c5d315656d8c822c8a0b9c7
FPT <lequynhan1212004@gmail.com>
2025-06-07 16:22:58 +0700

    Add CORS

--- app/backend.py
+++ app/backend.py
@@ -13,7 +13,7 @@ Instrumentator().instrument(app).expose(app)
 # Cấu hình CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],  # Chỉ cho phép frontend Vite
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "http://frontend:5173"],  # Chỉ cho phép frontend Vite
     allow_credentials=True,
     allow_methods=["*"],  # Cho phép tất cả các phương thức (GET, POST, PUT, DELETE...)
     allow_headers=["*"],  # Cho phép tất cả các headers
